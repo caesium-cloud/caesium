ab719f4a0c25f1eb66ffa2cb494e1cf81ccd7d07
Christopher Ryan <clryan22@gmail.com>
2020-06-20 09:46:48 -0400

    document logging package & set log level in environment

--- env/env.go
+++ env/env.go
@@ -3,20 +3,34 @@ package env
 import (
 	"github.com/caesium-dev/caesium/log"
 	"github.com/kelseyhightower/envconfig"
+	"github.com/pkg/errors"
 )
 
 // Variables stores all of the necessary set environment
 // variables as outlined in the Environment structure
-var Variables *Environment
+var variables *Environment
 
-func init() {
-	if err := envconfig.Process("caesium", Variables); err != nil {
-		log.Fatal("failed to process environment variables (%v)", err)
+// Process the environment variables set for caesium.
+func Process() (err error) {
+	if err = envconfig.Process("caesium", variables); err != nil {
+		return errors.Wrap(err, "failed to process environment variables")
 	}
+
+	// set the log level
+	if err = log.SetLevelFromString(variables.LogLevel); err != nil {
+		return errors.Wrap(err, "failed to set log level")
+	}
+
+	return
+}
+
+// Variables returns the processed environment variables.
+func Variables() Environment {
+	return *variables
 }
 
 // Environment defines the environment variables used
-// by caesium
+// by caesium.
 type Environment struct {
 	LogLevel string `default:"info"`
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.0.0
 	go.uber.org/zap v1.15.0
 )
