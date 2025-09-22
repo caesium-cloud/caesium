17713ee311c6966961d3eec54f9d38b65e00aa84
Christopher Ryan <cryan@Mac.localdomain>
2025-09-21 21:06:32 -0400

    lint

--- cmd/job/output.go
+++ cmd/job/output.go
@@ -1,6 +1,10 @@
 package job
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 func writeCmdOut(cmd *cobra.Command, format string, args ...any) error {
 	if _, err := fmt.Fprintf(cmd.OutOrStdout(), format, args...); err != nil {
