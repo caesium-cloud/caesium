1662141ea5aad2325759202de3f85247f5164b19
Christopher Ryan <clryan22@gmail.com>
2026-03-22 13:24:58 -0400

    default log console to on

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -37,7 +37,7 @@ func Variables() Environment {
 type Environment struct {
 	LogLevel                      string        `default:"info" split_words:"true"`
 	LogFormat                     string        `default:"json" split_words:"true"`
-	LogConsoleEnabled             bool          `default:"false" split_words:"true"`
+	LogConsoleEnabled             bool          `default:"true" split_words:"true"`
 	Port                          int           `default:"8080"`
 	DockerHost                    string        `default:"" split_words:"true"`
 	KubernetesConfig              string        `default:"" split_words:"true"`
